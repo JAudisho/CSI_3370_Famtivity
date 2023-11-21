6cd67121ccc910e08b82ddbc7bdd003e5b529918
Drewayou <49324678+Drewayou@users.noreply.github.com>
2023-11-20 20:00:10 -0500

    Database_Implementation 1.05
    Database structure and implementation is complete.
    
    Asynchronous assumption was correct as adding a 20ms buffer fixed the issue of res.render() activating before the API's retrieval of data.

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 var router = express.Router();
 const sqlite3 = require('sqlite3');
 
+//Possible implementation of npm better-sqlite3
+//const Database = require('better-sqlite3');
+//const db = require('better-sqlite3')('foobar.db', options);
+
 const app = express();
 const path = require('path');
 const http = require('http');
@@ -25,56 +29,45 @@ let child_db = new sqlite3.Database('./db/child_db_data.db', sqlite3.OPEN_READWR
     console.log('Connected to the child database resource... Awaiting input...');
   });
 
+//Load database into children_data object
+//Sets a global var to contain JSON
+var children_data = [];
+
+//children_data loadDb Method initiator
+function loadDb(){
+child_db.each('SELECT Child_First_Name, Child_Last_Name, Child_Age, Child_Description FROM child_db_data;',function (err, row){
+    if (err) {
+        res.status(400).json({ "Database data could not be retrieved!": err.message })
+        return;
+    }
+    children_data.push({
+        Child_First_Name: row.Child_First_Name,
+        Child_Last_Name: row.Child_Last_Name,
+        Child_Age: row.Child_Age,
+        Child_Description: row.Child_Description,
+    });
+});}
+
+//Calls "loadDb method to actually populate the db"
+loadDb();
+
 //DYNAMIC PAGE ROUTES :
 
 //GET Route below to populate Childen into the HTML/EJS, & loads the Children page
-
-    //Set global JSON variable
-    var children_data = [];
-
     app.get('/children.ejs', (req, res)=>{
-      
-    // Inserted Async function and let database = return : 
-    async function getDatabaseData() {
-      
-    //inserted try->catch
-    try {
-    let returnData = await child_db.serialize(()=>{
-        child_db.each('SELECT Child_First_Name, Child_Last_Name, Child_Age, Child_Description FROM child_db_data;',function (err, row){
-            if (err) {
-                res.status(400).json({ "Database data could not be retrieved!": err.message })
-                return;
-            }
-            children_data.push({
-                Child_First_Name: row.Child_First_Name,
-                Child_Last_Name: row.Child_Last_Name,
-                Child_Age: row.Child_Age,
-                Child_Description: row.Child_Description,
-            });
-        })
-    }); 
-        //Send JSON to client for rendering on ejs file -> added returnData?
-        res.render('children',{'children_data' : returnData.children_data} );
 
-        //Output JSON in totallity has been made to console sems to not go outside function
-        console.log(JSON.stringify(children_data));
-        
-        } //end of "try" here with "catch" below
-        catch (error) {
-          console.error('ERROR: ${error}');
-          }
-        
-        } //End of asynch here
-        
-        //Call Async Func
-        getDatabaseData();
+    loadDb();
 
-        //Reset passing JSON global var to empty to prevent looping append to JSON
-        children_data = [];
-        });
+    //Send JSON to client for rendering of Children.ejs file after 350ms buffer
+    setTimeout(function(){
+        res.render('children',{'children_data' : children_data});
+    },20);
+
+    //Output JSON in totallity to confirm data was sent
+    console.log(JSON.stringify(children_data));
+    });
 
 //POST Route below to populate Childen into the database, then re-loads the Children page
-
 app.post('/children.ejs', (req, res, next) => {
     child_db.serialize(()=>{
     child_db.run(`INSERT INTO child_db_data (Child_First_Name, Child_Last_Name, Child_Age, Child_Description) VALUES (?,?,?,?)`,[req.body.childFirstName,req.body.childLastName,req.body.childAge,req.body.childDesc],
@@ -88,28 +81,16 @@ app.post('/children.ejs', (req, res, next) => {
         });
     });
 
-    /*
-    child_db.serialize(()=>{
-        child_db.each('SELECT Child_First_Name, Child_Last_Name, Child_Age, Child_Description FROM child_db_data;',function (err, row){
-            if (err) {
-                res.status(400).json({ "Database data could not be retrieved!": err.message })
-                return;
-            }
-            children_data.push({
-                Child_First_Name: row.Child_First_Name,
-                Child_Last_Name: row.Child_Last_Name,
-                Child_Age: row.Child_Age,
-                Child_Description: row.Child_Description,
-            });
-            
-        });
-    }
-    */
+    loadDb();
 
-    //Send JSON to client for rendering on ejs file
-    res.render('children',{'children_data' : children_data});
+    //Send JSON to client for rendering of Children.ejs file after 350ms buffer
+    setTimeout(function(){
+        res.render('children',{'children_data' : children_data});
+    },20);
 
-    });
+    //Output JSON in totallity to confirm data was sent
+    console.log(JSON.stringify(children_data));
+});
 
 //Load the initial index page
 app.get('/', (req, res)=>{ 
