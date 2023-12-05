4c5c1f536beb34ceca67ddd534cd895134541e70
anthony aljida <anthonyaljida@icloud.com>
2023-12-05 10:48:55 -0500

    "Created a working local storage for events"

--- public/assets/scripts/event.js
+++ public/assets/scripts/event.js
@@ -1,17 +1,38 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const form = document.querySelector("form");
-    const output = document.querySelector(".output");
-  
-    form.addEventListener("submit", function (event) {
-      event.preventDefault();
-  
-      // Assuming you have a function saveAndClear in children.js
-      saveAndClear();
-  
-      // Additional logic for scheduling activities can go here
-  
-      // You might want to update the output or perform other actions
-      // based on the scheduling logic
-    });
+const storage2 = localStorage.getItem("events");
+const form = document.querySelector("form");
+const output = document.getElementById("scheduleButton");
+if (storage2) {
+  previousEvents = JSON.parse(storage2);
+  console.log(previousEvents);
+  previousEvents.forEach((element) => {
+    const markUp = `<div style="background-color: #7FFFD4;">
+    <p>${element[0]}</p>
+    <p>${element[1]}</p>
+    <p>${element[2]}</p>
+    <p>${element[3]}</p>
+    <p>${element[4]}</p>
+  </div>`;
+    output.insertAdjacentHTML("afterend", markUp);
   });
-  
+}
+
+let storage = [];
+output.addEventListener("click", function (event) {
+  const formElements = Array.from(document.querySelectorAll(".form-control"));
+  const valueOfElements = formElements.map((element) => element.value);
+  event.preventDefault();
+  const markUp = `<div style="background-color: #7FFFD4;">
+    <p>${valueOfElements[0]}</p>
+    <p>${valueOfElements[1]}</p>
+    <p>${valueOfElements[2]}</p>
+    <p>${valueOfElements[3]}</p>
+    <p>${valueOfElements[4]}</p>
+  </div>`;
+  output.insertAdjacentHTML("afterend", markUp);
+  storage.push(valueOfElements);
+  localStorage.setItem("events", JSON.stringify(storage));
+});
+
+const clearBookmarks = function () {
+  localStorage.clear("bookmarks");
+};
